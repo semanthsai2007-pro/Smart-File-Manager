cf611e2ef6ad5d2fb0a3b43ab4632038724ca97e
P.Semanth Sai <semanthsai2007@gmail.com>
2026-07-29 18:41:53 +0530

    Successfully Completed Rename-Function

--- file_manager.py
+++ file_manager.py
@@ -23,7 +23,38 @@ class FileManager:
             print("File Not Found")
 
     def rename_file(self):
-        print("Rename Module Opened")
+        folder = input("Enter Folder Path: ")
+        filename = input("Enter Old File Name: ")
+        new_filename = input("Enter New File Name: ")
+
+        print("\nFolder Path:", folder)
+        print("Searching for:", filename)
+
+        found = False
+
+        for root, dirs, files in os.walk(folder):
+            for file in files:
+                if filename == file:
+                    found = True
+
+                    old_path = os.path.join(root, file)
+                    new_path = os.path.join(root, new_filename)
+
+                    try:
+                        os.rename(old_path, new_path)
+                        print("\nFile Renamed Successfully!")
+                        print(f"'{filename}' renamed to '{new_filename}'")
+
+                    except Exception as e:
+                        print("\nError:", e)
+
+                    break
+
+            if found:
+                break
+
+        if not found:
+            print("\nFile Not Found")
 
     def move_file(self):
         print("Move Module Opened")
